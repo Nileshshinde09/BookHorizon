2a717ed707a92065c91e507a47c1c9baa913db68
Nilesh Shinde <90444068+Nileshshinde09@users.noreply.github.com>
2023-08-03 09:49:10 +0530

    Update bookdetails.js

--- components/bookdetails.js
+++ components/bookdetails.js
@@ -1,7 +1,7 @@
 export const getBookDetails = async(isbn) => {
     
     try{
-        const url = `https://bookhorizonbackend.vercel.app/getDetialsOnISBN/${isbn}`
+        const url = `https://bookhorizonbackend.vercel.app/getDetialsOnISBN?ISBN=${isbn}`
         const response = await fetch(url);
         const result = await response.json();
         return result
